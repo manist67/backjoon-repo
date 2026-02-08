cc25cc42f2affd82627868450d48f554d8ccc349
김철각 <manist67@naver.com>
2026-02-09 00:57:20 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 64 ms, Memory: 1068 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.go
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.go
@@ -1,39 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var n int
 	fmt.Scanf("%d\n", &n)
 
-	var arr []string
+	var arr [][]rune
 	for i := 0; i < n; i++ {
 		var s string
 		fmt.Scanf("%s\n", &s)
-		arr = append(arr, s)
+		arr = append(arr, []rune(s))
 	}
 
 	l := len(arr[0])
 
-	ans := ""
+	var ans strings.Builder
+
 	for i := 0; i < l; i++ {
 		isAllCorrect := true
 
-		controlChar := string(arr[0][i])
+		controlChar := arr[0][i]
 
 		for j := 1; j < n; j++ {
-			if controlChar != string(arr[j][i]) {
+			if controlChar != arr[j][i] {
 				isAllCorrect = false
 				break
 			}
 		}
 
 		if isAllCorrect {
-			ans += controlChar
+			ans.WriteRune(controlChar)
 		} else {
-			ans += "?"
+			ans.WriteString("?")
 		}
 	}
 
-	fmt.Printf("%s\n", ans)
+	fmt.Printf("%s\n", ans.String())
 }
