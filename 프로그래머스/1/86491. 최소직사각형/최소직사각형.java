8225c62dca32d7f2c1023cb521c6a3e5b2aa22e3
김철각 <manist67@naver.com>
2026-04-04 20:01:49 +0900

    [level 1] Title: 최소직사각형, Time: 1.85 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,34 +1,16 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-
 class Solution {
     public int solution(int[][] sizes) {
-        int len = sizes.length * 2;
-        HashSet<Integer> candidates = new HashSet<>();
+        int maxWidth = 0;
+        int maxHeight = 0;
         
-        
-        for(int[] s : sizes) {
-            candidates.add(s[0]);
-            candidates.add(s[1]);
-        }
-        
-        int answer = 1000 * 1000 * 2;
-        for(int x : candidates) {            
-            for(int y : candidates) {
-                if(isValid(x, y, sizes) && answer > x*y) answer = x*y;
-            }
-        }
-        
-        return answer;
-    }
-    
-    private boolean isValid(int x, int y, int[][] sizes) {
         for(int[] size : sizes) {
-            if((x >= size[0] && y >= size[1]) || (x >= size[1] && y >= size[0])) continue;
-            return false;
+            int w = Math.max(size[0], size[1]);
+            int h = Math.min(size[0], size[1]);
+            
+            maxWidth = Math.max(maxWidth, w);
+            maxHeight = Math.max(maxHeight, h);
         }
         
-        return true;
-    }
-    
+        return maxWidth * maxHeight;
+    }    
 }
